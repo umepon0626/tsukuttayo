750110628b9613a18a03ee0ccdf87b945d1fa23a
umepon0626 <ryuichiastrona@gmail.com>
2021-02-10 20:22:36 +0900

    delete route

--- workplace/src/App.js
+++ workplace/src/App.js
@@ -1,43 +1,19 @@
 import React from "react";
-import {
-  BrowserRouter as Router,
-  Switch,
-  Route,
-  Link,
-  useRouteMatch,
-  useParams
-} from "react-router-dom";
 
 export default function App() {
   console.log('welcome!')
   return (
-    <Router>
-      <div>
         <ul>
           <li>
-            <Link to="/">Home</Link>
+          <h1>Home</h1>
           </li>
           <li>
-            <Link to="/about">About</Link>
+          <h1>Home</h1>
           </li>
           <li>
-            <Link to="/topics">Topics</Link>
+          <h1>Home</h1>
           </li>
         </ul>
-
-        <Switch>
-          <Route path="/about">
-            <About />
-          </Route>
-          <Route path="/topics">
-            <Topics />
-          </Route>
-          <Route component={Home}>
-            <Home />
-          </Route>
-        </Switch>
-      </div>
-    </Router>
   );
 }
 
@@ -49,41 +25,3 @@ function About() {
   return <h2>About</h2>;
 }
 
-function Topics() {
-  let match = useRouteMatch();
-
-  return (
-    <div>
-      <h2>Topics</h2>
-
-      <ul>
-        <li>
-          <Link to={`${match.url}/components`}>Components</Link>
-        </li>
-        <li>
-          <Link to={`${match.url}/props-v-state`}>
-            Props v. State
-          </Link>
-        </li>
-      </ul>
-
-      {/* The Topics page has its own <Switch> with more routes
-          that build on the /topics URL path. You can think of the
-          2nd <Route> here as an "index" page for all topics, or
-          the page that is shown when no topic is selected */}
-      <Switch>
-        <Route path={`${match.path}/:topicId`}>
-          <Topic />
-        </Route>
-        <Route path={match.path}>
-          <h3>Please select a topic.</h3>
-        </Route>
-      </Switch>
-    </div>
-  );
-}
-
-function Topic() {
-  let { topicId } = useParams();
-  return <h3>Requested topic ID: {topicId}</h3>;
-}
