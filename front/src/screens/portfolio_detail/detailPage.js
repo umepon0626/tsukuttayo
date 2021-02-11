59c46402344b36d90b3ea533327e57fb29695728
umepon0626 <ryuichiastrona@gmail.com>
2021-02-11 17:17:11 +0900

    add route

---
+++ front/src/screens/portfolio_detail/detailPage.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import {Navbar} from 'react-bootstrap'
+
+export default function DetailPage(){
+  return(
+    <Navbar fixed='top' bg='light'>
+      User情報
+    </Navbar>
+  )
+}
